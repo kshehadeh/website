bebca3de055c1baf49c060e4e4592bf3338470d3
Karim Shehadeh <kshehadeh@underarmour.com>
2024-02-01 18:04:30 -0500

    Add an invalidation endpoint

---
+++ app/api/invalidate/[...path]/route.ts
@@ -0,0 +1,15 @@
+import { revalidatePath } from "next/cache";
+
+export async function GET(req: Request, {params: { path }}: { params: { path: string[] } }) {
+
+    const query = new URLSearchParams(req.url.split('?')[1] || '')
+    if (query.get('secret') !== process.env.REVALIDATION_SECRET) {
+        return new Response('Unauthorized', { status: 401 });
+    }
+
+    const pathToRevalidate = `/${path.join('/')}`;
+    revalidatePath(pathToRevalidate);
+
+    return new Response(JSON.stringify({ revalidated: true, path: pathToRevalidate }), { status: 200, headers: { 'Content-Type': 'application/json' } });
+}
+

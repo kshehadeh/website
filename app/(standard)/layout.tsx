98dbbf866b6786b8729f8ed51a08211376f3b859
Karim Shehadeh <kshehadeh@underarmour.com>
2024-02-26 05:09:39 -0500

    Added robots.txt

--- app/(standard)/layout.tsx
+++ app/(standard)/layout.tsx
@@ -7,8 +7,8 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-    title: 'Karim Shehadeh',
-    description: "Karim Shehadeh's personal website and blog.",
+    title: 'Karim Shehadeh\'s Personal Site',
+    description: "Karim Shehadeh's personal website that includes a blog, links I have come across, resume and some helpful tooling.",
 };
 
 export default function RootLayout({

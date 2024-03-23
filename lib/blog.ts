983294f89ebd1a22435db63c77678ad2d3950e31
Karim Shehadeh <kshehadeh@underarmour.com>
2024-03-23 18:31:26 -0400

    Change the look of the post cards

--- lib/blog.ts
+++ lib/blog.ts
@@ -60,7 +60,7 @@ export function getAbstractFromBlocks(blocks: BlockObjectResponse[]): string {
             .filter(isParagraphBlockObject)
             .map(p => p.paragraph.rich_text[0]?.plain_text)
             .join(' ')
-            .slice(0, 200) + '...'
+            .slice(0, 500) + '...'
     );
 }
 

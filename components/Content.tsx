f7371267eee37db3892685a96d8399264ad0f5d7
Karim Shehadeh <kshehadeh@underarmour.com>
2024-05-26 19:05:33 -0400

    Fixed a couple of issues with rendering blog posts

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Content({ children }: React.PropsWithChildren) {
     return (
-        <main className="prose max-w-none bg-white md:ml-5 md:mr-5 md:pl-10 md:pr-10 rounded-md ml-2 pl-2 mr-2 pr-2">
+        <main className="prose max-w-none bg-white md:ml-5 md:mr-5 md:pl-10 md:pr-10 rounded-md ml-2 pl-2 mr-2 pr-2 pb-6">
             {children}
         </main>
     );

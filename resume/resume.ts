c5c730bd9c88a39f1515c67a6169a8835d432069
Karim Shehadeh <kshehadeh@underarmour.com>
2025-01-02 04:56:30 -0500

    Added manual run trigger for resume builder action

--- resume/resume.ts
+++ resume/resume.ts
@@ -1,9 +1,13 @@
 import { Client } from '@notionhq/client';
-import type { BlockObjectResponse, ChildDatabaseBlockObjectResponse, PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+import type {
+    BlockObjectResponse,
+    ChildDatabaseBlockObjectResponse,
+    PageObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints';
 import { config } from 'dotenv';
 import { DateTime, Duration } from 'luxon';
 import path from 'path';
-import { q } from '@kshehadeh/cmdq'
+import { q } from '@kshehadeh/cmdq';
 import fs from 'fs';
 import ora from 'ora';
 
@@ -13,7 +17,7 @@ config();
 
 // Initialize the Notion client
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
-const spinner = ora('Building resume...').start()
+const spinner = ora('Building resume...').start();
 
 function toHuman(dt: Duration | undefined | null, andCounting: boolean) {
     if (!dt) {
@@ -22,11 +26,13 @@ function toHuman(dt: Duration | undefined | null, andCounting: boolean) {
 
     const pluralize = (unit: string, count: number) => {
         return count === 1 ? unit : `${unit}s`;
-    }
+    };
 
-    let str = ''
+    let str = '';
     if (dt.years > 0 && dt.months > 0) {
-        str = dt.toFormat(`y '${pluralize('year', dt.years)}' M '${pluralize('month', dt.months)}'`);
+        str = dt.toFormat(
+            `y '${pluralize('year', dt.years)}' M '${pluralize('month', dt.months)}'`,
+        );
     } else if (dt.years > 0) {
         str = dt.toFormat(`y '${pluralize('year', dt.years)}'`);
     } else if (dt.months > 0) {
@@ -34,7 +40,7 @@ function toHuman(dt: Duration | undefined | null, andCounting: boolean) {
     }
 
     if (andCounting) {
-        str = "Current Position"
+        str = 'Current Position';
     }
 
     return str;
@@ -44,15 +50,18 @@ function roundMonths(dt: Duration) {
     if (dt.months > 0) {
         const rounded = Math.round(dt.months);
         if (rounded === 0) {
-            return Duration.fromObject({years: dt.years});
+            return Duration.fromObject({ years: dt.years });
         } else {
             return dt.set({ months: rounded });
         }
     }
-    return dt
+    return dt;
 }
 
-const sortPropertiesByEndDates = (a: PageObjectResponse, b: PageObjectResponse) => {
+const sortPropertiesByEndDates = (
+    a: PageObjectResponse,
+    b: PageObjectResponse,
+) => {
     const aEnd = getPropertyDate(a.properties.Dates).end;
     const bEnd = getPropertyDate(b.properties.Dates).end;
 
@@ -69,7 +78,7 @@ const sortPropertiesByEndDates = (a: PageObjectResponse, b: PageObjectResponse) 
     }
 
     return 0;
-}
+};
 
 async function getBlocks(id: string) {
     try {
@@ -88,7 +97,7 @@ async function getRows(databaseId: string) {
         const response = await notion.databases.query({
             database_id: databaseId,
         });
-        return response.results.filter((o) => o.object === 'page');
+        return response.results.filter(o => o.object === 'page');
     } catch (error) {
         console.error('Error fetching rows:', error);
         throw error;
@@ -111,24 +120,28 @@ function getProperyNumber(property: PageObjectResponse['properties'][number]) {
     return null;
 }
 
-function getPropertyMultiSelect(property: PageObjectResponse['properties'][number]) {
+function getPropertyMultiSelect(
+    property: PageObjectResponse['properties'][number],
+) {
     if (property.type === 'multi_select') {
-        return property.multi_select.map((option) => option.name);
+        return property.multi_select.map(option => option.name);
     }
     return null;
 }
 
 function getAboutValueWithKey(rows: PageObjectResponse[], key: string) {
-    const row = rows.find((row) => getPropertyText(row.properties.Name) === key);
+    const row = rows.find(row => getPropertyText(row.properties.Name) === key);
     if (!row) {
         return null;
     }
     return getPropertyText(row.properties.Text);
 }
 
-async function buildAboutInfo(data: Record<string, ChildDatabaseBlockObjectResponse>) {
+async function buildAboutInfo(
+    data: Record<string, ChildDatabaseBlockObjectResponse>,
+) {
     const dbId = data['About'].id;
-    const rows = await getRows(dbId) as PageObjectResponse[];
+    const rows = (await getRows(dbId)) as PageObjectResponse[];
     return {
         intro: getAboutValueWithKey(rows, 'Intro'),
         tagline: getAboutValueWithKey(rows, 'Tagline'),
@@ -139,7 +152,7 @@ async function buildAboutInfo(data: Record<string, ChildDatabaseBlockObjectRespo
         phone: getAboutValueWithKey(rows, 'Phone'),
         email: getAboutValueWithKey(rows, 'Email'),
         name: getAboutValueWithKey(rows, 'Name'),
-    }
+    };
 }
 
 function getPropertyDate(property: PageObjectResponse['properties'][number]) {
@@ -147,38 +160,60 @@ function getPropertyDate(property: PageObjectResponse['properties'][number]) {
         if (property.date?.start && property.date?.end) {
             const start = DateTime.fromISO(property.date.start);
             const end = DateTime.fromISO(property.date.end);
-            const duration = roundMonths(end.diff(start, ["years", "months"], { conversionAccuracy: "longterm" }));
+            const duration = roundMonths(
+                end.diff(start, ['years', 'months'], {
+                    conversionAccuracy: 'longterm',
+                }),
+            );
             return {
                 start,
                 end,
-                duration
+                duration,
             };
         }
 
         if (property.date?.start) {
-            const duration = roundMonths(DateTime.now().diff(DateTime.fromISO(property.date.start), ["years", "months"], { conversionAccuracy: "longterm" }));
+            const duration = roundMonths(
+                DateTime.now().diff(
+                    DateTime.fromISO(property.date.start),
+                    ['years', 'months'],
+                    { conversionAccuracy: 'longterm' },
+                ),
+            );
             return { start: DateTime.fromISO(property.date?.start), duration };
-        } else if (property.date?.end) {            
+        } else if (property.date?.end) {
             return { end: DateTime.fromISO(property.date?.end) };
         }
     }
     return { start: null, end: null, duration: null };
 }
 
-async function buildExperienceList(data: Record<string, ChildDatabaseBlockObjectResponse>) {
+async function buildExperienceList(
+    data: Record<string, ChildDatabaseBlockObjectResponse>,
+) {
     spinner.text = 'Reading company data';
-    const companies = await getRows(data['Companies'].id) as PageObjectResponse[]
+    const companies = (await getRows(
+        data['Companies'].id,
+    )) as PageObjectResponse[];
 
     spinner.text = 'Reading experience data';
-    const experience = await getRows(data['Experience'].id) as PageObjectResponse[]
-    const experienceBullets = await getRows(data['Experience Bullets'].id) as PageObjectResponse[]
+    const experience = (await getRows(
+        data['Experience'].id,
+    )) as PageObjectResponse[];
+    const experienceBullets = (await getRows(
+        data['Experience Bullets'].id,
+    )) as PageObjectResponse[];
 
     companies.sort(sortPropertiesByEndDates).reverse();
 
-    const experienceList = companies.map((company) => {
+    const experienceList = companies.map(company => {
         spinner.text = `Processing ${getPropertyText(company.properties.Name)}`;
 
-        const expForCompany = experience.filter((exp) => (exp.properties.Company.type === "relation" && exp.properties.Company.relation[0].id) === company.id)
+        const expForCompany = experience.filter(
+            exp =>
+                (exp.properties.Company.type === 'relation' &&
+                    exp.properties.Company.relation[0].id) === company.id,
+        );
         expForCompany.sort(sortPropertiesByEndDates).reverse();
 
         const { start, end } = getPropertyDate(company.properties.Dates);
@@ -186,36 +221,49 @@ async function buildExperienceList(data: Record<string, ChildDatabaseBlockObject
             company: getPropertyText(company.properties.Name),
             start: start?.toFormat('MMMM yyyy'),
             end: end?.toFormat('MMMM yyyy') ?? 'Present',
-            experience: expForCompany.map((exp) => {
+            experience: expForCompany.map(exp => {
                 spinner.text = `Processing ${getPropertyText(exp.properties.Name)}`;
                 const bulletsForExperience = experienceBullets
-                    .filter((bullet) => (bullet.properties.Experience.type === "relation" && bullet.properties.Experience.relation[0].id) === exp.id)
+                    .filter(
+                        bullet =>
+                            (bullet.properties.Experience.type === 'relation' &&
+                                bullet.properties.Experience.relation[0].id) ===
+                            exp.id,
+                    )
                     .reverse();
-                const { start, end, duration } = getPropertyDate(exp.properties.Dates);
+                const { start, end, duration } = getPropertyDate(
+                    exp.properties.Dates,
+                );
                 return {
                     title: getPropertyText(exp.properties.Name),
                     start: start?.toFormat('MMMM yyyy'),
                     end: end?.toFormat('MMMM yyyy') ?? 'Present',
                     duration: toHuman(duration, !end),
-                    bullets: bulletsForExperience.map((bullet) => {
+                    bullets: bulletsForExperience.map(bullet => {
                         return {
                             text: getPropertyText(bullet.properties.Name),
-                            skills: getPropertyMultiSelect(bullet.properties.Skills),
-                        }
-                    })
-                }
-            })
-        }
-    })
+                            skills: getPropertyMultiSelect(
+                                bullet.properties.Skills,
+                            ),
+                        };
+                    }),
+                };
+            }),
+        };
+    });
 
     return experienceList;
 }
 
-async function buildEducationList(data: Record<string, ChildDatabaseBlockObjectResponse>) {
+async function buildEducationList(
+    data: Record<string, ChildDatabaseBlockObjectResponse>,
+) {
     spinner.text = 'Reading education data';
-    const education = await getRows(data['Education'].id) as PageObjectResponse[]
+    const education = (await getRows(
+        data['Education'].id,
+    )) as PageObjectResponse[];
 
-    const educationList = education.map((edu) => {
+    const educationList = education.map(edu => {
         spinner.text = `Processing ${getPropertyText(edu.properties.Name)}`;
         const { start, end } = getPropertyDate(edu.properties.GraduationDate);
 
@@ -227,8 +275,8 @@ async function buildEducationList(data: Record<string, ChildDatabaseBlockObjectR
             end: end?.toFormat('MMMM yyyy') ?? 'Present',
             awards: getPropertyMultiSelect(edu.properties.Awards),
             gpa: getProperyNumber(edu.properties.GPA),
-        }
-    })
+        };
+    });
 
     return educationList;
 }
@@ -240,9 +288,9 @@ async function buildEducationList(data: Record<string, ChildDatabaseBlockObjectR
     if (!outputFile) {
         outputFile = 'karim-shehadeh-resume.pdf';
     }
-    
+
     outputFile = path.resolve('./', outputFile);
-    
+
     // check if directory exists
     if (!fs.existsSync(path.dirname(outputFile))) {
         console.error(`Directory ${path.dirname(outputFile)} does not exist`);
@@ -252,10 +300,15 @@ async function buildEducationList(data: Record<string, ChildDatabaseBlockObjectR
     const documentOrDatabaseId = 'b8fed2f709c64d139c1f5a77c469d511';
     const blocks = await getBlocks(documentOrDatabaseId);
 
-    const databases = blocks.filter(block => block.type === 'child_database').reduce<Record<string, ChildDatabaseBlockObjectResponse>>((acc, block) => {
-        acc[block.child_database.title] = block;
-        return acc;
-    }, {});
+    const databases = blocks
+        .filter(block => block.type === 'child_database')
+        .reduce<Record<string, ChildDatabaseBlockObjectResponse>>(
+            (acc, block) => {
+                acc[block.child_database.title] = block;
+                return acc;
+            },
+            {},
+        );
 
     const experiences = await buildExperienceList(databases);
     const education = await buildEducationList(databases);
@@ -272,18 +325,23 @@ async function buildEducationList(data: Record<string, ChildDatabaseBlockObjectR
         web: about.web,
         location: about.location,
         experiences,
-        education
-    }
+        education,
+    };
 
     const outputPath = path.join(rootDir, '/resume.json');
     fs.writeFileSync(outputPath, JSON.stringify(data, null, 2));
 
     const pathToTemplate = path.join(rootDir, 'main.typ');
-    const output = q().add(`typst compile ${pathToTemplate} ${outputFile}`).run().last();
+    const output = q()
+        .add(`typst compile ${pathToTemplate} ${outputFile}`)
+        .run()
+        .last();
     if (!output?.result.success) {
-        console.log(output?.result.message || `Error compiling template (error code ${output?.stat})`);
+        console.log(
+            output?.result.message ||
+                `Error compiling template (error code ${output?.stat})`,
+        );
     }
 
     spinner.stopAndPersist({ text: 'Resume built successfully' });
-
-})();   
+})();
